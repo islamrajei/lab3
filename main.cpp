6511be90a6880769c07accd13ce5633152ea593d
nplatt2 <70676144+nplatt2@users.noreply.github.com>
2020-09-04 23:03:18 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -31,3 +31,15 @@ cout << "hello World " << name1 << endl;
 return 0;
 }
 hello1Function
+
+/*
+ * hello2Function.cpp
+ *
+ */
+#include <iostream>
+using namespace std;
+int hello2Function(string name2)
+{
+  cout << "hello World " << name2 << endl;
+  return 0;
+}
